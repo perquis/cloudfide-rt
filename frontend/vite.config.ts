0ff4761fff81f2a919fcb9621efdc8bb640bc774
perquis <thetahirpl@gmail.com>
2024-11-08 11:50:50 +0100

    :wrench: config(vitejs): Added server proxy to frontend application.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,13 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api/v3": {
+        target: "http://localhost:3000/api",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
