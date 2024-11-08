478f52b149babcf3e57fce841901757ae42d400a
perquis <thetahirpl@gmail.com>
2024-11-08 11:02:50 +0100

    :fire: prune(mongodb): Removed function to connect with mongodb because it is unnecessary.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+
 /*
 Etap planowania
 
-1. Pobranie danych w celu zidentyfikowania zmian cen kryptowalut, czy kwota została zwiększona, bądź zmniejszona
-    - cron do pobierania danych co jeden dzień i wysyłanie tych informacji do bazy danych MongoDb
-2. Wyświetlenie danych w jakimś widoku
+1. Wyświetlenie danych w jakimś widoku
     - endpoint GET /api/historical-trades ---> TradeResult[]
     - zainstalowanie modułu supertest ---> testowanie kontrolera ---> GET /api/historical-trades
     - nastepnie przygotować widok, który umożliwi wyświetlenie wyników na jakimś wykresie po stronie frontendu (REACT, react-charts, react-query od TanStack)
+        - utworzyć serwer proxy po stronie frontendu, aby nie było potrzeby konfiguracji polityki CORS
 */
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
