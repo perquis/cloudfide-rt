a0dbbd70662fcff7779b1b7f7ed332bb26d27254
perquis <thetahirpl@gmail.com>
2024-11-08 10:34:58 +0100

    :memo: docs(main): Updated comments to add more tasks.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ Etap planowania
     - cron do pobierania danych co jeden dzień i wysyłanie tych informacji do bazy danych MongoDb
 2. Wyświetlenie danych w jakimś widoku
     - endpoint GET /api/historical-trades ---> TradeResult[]
-    
-
+    - zainstalowanie modułu supertest ---> testowanie kontrolera ---> GET /api/historical-trades
+    - nastepnie przygotować widok, który umożliwi wyświetlenie wyników na jakimś wykresie po stronie frontendu (REACT, react-charts, react-query od TanStack)
 */
