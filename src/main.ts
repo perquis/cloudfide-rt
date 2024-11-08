2b6ac4e764c46d7c926b31b73bb26a3578c65af7
perquis <thetahirpl@gmail.com>
2024-11-08 10:29:45 +0100

    :memo: docs(main): Added comments to plan what to do for this task.

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+/*
+
+Etap planowania
+
+1. Pobranie danych w celu zidentyfikowania zmian cen kryptowalut, czy kwota została zwiększona, bądź zmniejszona
+    - cron do pobierania danych co jeden dzień i wysyłanie tych informacji do bazy danych MongoDb
+2. Wyświetlenie danych w jakimś widoku
+    - endpoint GET /api/historical-trades ---> TradeResult[]
+    
+
+*/
