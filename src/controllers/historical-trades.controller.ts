6bb12c5c959b989cd46ffe82ea0c9ba6c91bffaa
perquis <thetahirpl@gmail.com>
2024-11-08 11:27:15 +0100

    :memo: docs(historical-trades): Added task to do later.

--- src/controllers/historical-trades.controller.ts
+++ src/controllers/historical-trades.controller.ts
@@ -7,7 +7,10 @@ export const historicalTradesController = Router();
 
 historicalTradesController.get("/:symbol", async (req, res) => {
   const { symbol } = req.params;
-  const historicalTrades = await binance.getHistoricalTrades({ symbol });
+  const historicalTrades = await binance.getHistoricalTrades({ symbol: symbol.toUpperCase() });
+
+  // TODO: validate symbol with enum
+  // TODO: time range for this historical trades
 
   res.json(historicalTrades);
 });
--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,6 @@ app.use(errorMiddleware);
 Etap planowania
 
 1. Wyświetlenie danych w jakimś widoku
-    - endpoint GET /api/historical-trades ---> TradeResult[]
     - zainstalowanie modułu supertest ---> testowanie kontrolera ---> GET /api/historical-trades
     - nastepnie przygotować widok, który umożliwi wyświetlenie wyników na jakimś wykresie po stronie frontendu (REACT, react-charts, react-query od TanStack)
         - utworzyć serwer proxy po stronie frontendu, aby nie było potrzeby konfiguracji polityki CORS
