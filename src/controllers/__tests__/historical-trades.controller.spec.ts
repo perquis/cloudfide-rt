c2c580258ebfddc72db6c4060aa888868f9941d7
perquis <thetahirpl@gmail.com>
2024-11-08 11:47:03 +0100

    :white_check_mark: tests(historical-trades): Updated this unit test and added new scenarios to check different use cases.

--- src/controllers/__tests__/historical-trades.controller.spec.ts
+++ src/controllers/__tests__/historical-trades.controller.spec.ts
@@ -12,10 +12,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/historical-trades", historicalTradesController);
 
 describe("GET /historical-trades", () => {
-  it("responds with json in RawTrade[] type", async () => {
-    const response = await request(app).get("/historical-trades/btcusdt").set("Accept", "application/json");
+  it("should return json that it has 500 trades list", async () => {
+    const response = await request(app).get("/historical-trades/BTCUSDT").set("Accept", "application/json");
 
     expect(response.status).toBe(200);
     expect(response.body.length).toBe(500);
+  });
+
+  it("should return json when symbol is with lower case", async () => {
+    const response = await request(app).get("/historical-trades/ethusdt").set("Accept", "application/json");
+    expect(response.status).toBe(200);
+  });
+
+  it("should return error when symbol is invalidated", async () => {
+    const response = await request(app).get("/historical-trades/not-existing-symbol").set("Accept", "application/json");
+    expect(response.status).toBe(500);
+
+    // TODO: Add specific error with determined message.
   });
 });
